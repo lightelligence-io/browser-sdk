9039c03809f6a49cef73937adb805699aa1306e8
Roman Makovyak <r.makovyak@osram.com>
2018-09-28 10:33:44 +0000

    Merged in minified-versions (pull request #22)
    Extend webpack to generate both development and production dist
    
    Approved-by: Jonathan Stoye <j.stoye@osram.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,78 @@
-const path = require('path');
-const spreadPlugin = require('babel-plugin-transform-object-rest-spread');
-const transformRuntimePlugin = require('babel-plugin-transform-runtime');
+const path = require("path");
+const spreadPlugin = require("babel-plugin-transform-object-rest-spread");
+const transformRuntimePlugin = require("babel-plugin-transform-runtime");
 
 const baseConfig = {
-  devtool: 'source-map',
-  entry: './src/index-web.js',
-  mode: 'development',
+  entry: "./src/index-web.js",
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'eslint-loader',
-        enforce: 'pre',
+        loader: "eslint-loader",
+        enforce: "pre"
       },
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['env'],
-            plugins: [spreadPlugin, transformRuntimePlugin],
-          },
-        },
-      },
-    ],
-  },
+            presets: ["env"],
+            plugins: [spreadPlugin, transformRuntimePlugin]
+          }
+        }
+      }
+    ]
+  }
 };
 
-const nodeConfig = {
+const nodeConfigDev = {
   ...baseConfig,
-  target: 'node',
+  devtool: "source-map",
+  mode: "development",
+  target: "node",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.node.js',
-  },
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.node.js"
+  }
 };
 
-const webConfig = {
+const nodeConfigProd = {
   ...baseConfig,
-  target: 'web',
+  mode: "production",
+  target: "node",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.web.js',
-    library: 'olt-js-sdk',
-    libraryTarget: 'umd',
-    umdNamedDefine: true,
-  },
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.node.min.js"
+  }
 };
 
-module.exports = [nodeConfig, webConfig];
+const webConfigDev = {
+  ...baseConfig,
+  devtool: "source-map",
+  mode: "development",
+  target: "web",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.web.js",
+    library: "olt-js-sdk",
+    libraryTarget: "umd",
+    umdNamedDefine: true
+  }
+};
+
+const webConfigProd = {
+  ...baseConfig,
+  mode: "production",
+  target: "web",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.web.min.js",
+    library: "olt-js-sdk",
+    libraryTarget: "umd",
+    umdNamedDefine: true
+  }
+};
+
+module.exports = [nodeConfigDev, nodeConfigProd, webConfigDev, webConfigProd];
